58d7a3035f7d2ab9e437187b7c58648d9da678f7
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-01 22:57:48 +0600

    Question 3: Username Gatekeeper

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-function getDayType(day) {
-    switch (day.toLowerCase()) {
-        case "friday":
-        case "saturday":
-            return "Weekend";
 
-        case "sunday":
-        case "monday":
-        case "tuesday":
-        case "wednesday":
-        case "thursday":
-            return "Working Day";
 
-        default:
-            return "Invalid Day";
+// Question 3: Username Gatekeeper
+function validateUsername(username) {
+    if (username.length < 4) {
+        return "Too Short";
     }
+
+    if (username.includes(" ")) {
+        return "No Space Allowed";
+    }
+
+    if (username.toLowerCase().includes("admin")) {
+        return "Reserved Word";
+    }
+
+    return "Available";
 }
+
+
+
+
+
+
+
+// // Question 2: Bangladesh Weekend Machine
+// function getDayType(day) {
+//     switch (day.toLowerCase()) {
+//         case "friday":
+//         case "saturday":
+//             return "Weekend";
+
+//         case "sunday":
+//         case "monday":
+//         case "tuesday":
+//         case "wednesday":
+//         case "thursday":
+//             return "Working Day";
+
+//         default:
+//             return "Invalid Day";
+//     }
+// }
 
 
 
