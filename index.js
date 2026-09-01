f16f3e298868cb286e047bfc85f33d6b36618eb0
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-01 22:54:55 +0600

    Question 2: Bangladesh Weekend Machine

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+function getDayType(day) {
+    switch (day.toLowerCase()) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
 
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
 
-// Question 1: 
-function describeValue(value) {
-    const type = typeof value;
-    const truthiness = value ? "truthy" : "falsy";
-
-    return `${type} | ${truthiness}`;
+        default:
+            return "Invalid Day";
+    }
 }
+
+
+
+
+
+// // Question 1: 
+// function describeValue(value) {
+//     const type = typeof value;
+//     const truthiness = value ? "truthy" : "falsy";
+
+//     return `${type} | ${truthiness}`;
+// }
 
