192cc2bcca2de14ebd764561b86f6d7140460a4f
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-02 18:37:17 +0600

    Question 4: Dhaka CNG Fare Meter

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 
 
+
+// Question 4: Dhaka CNG Fare Meter
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+    let fare = 50;
+
+    if (distance > 2) {
+        fare += (distance - 2) * 15;
+    }
+
+    fare += waitingMinutes * 2;
+
+    if (isNight) {
+        fare = fare * 1.20;
+    }
+
+    return fare;
+}
+
+
+
 // Question 3: Username Gatekeeper
 function validateUsername(username) {
     if (username.length < 4) {
@@ -23,34 +43,34 @@ function validateUsername(username) {
 
 
 
-// // Question 2: Bangladesh Weekend Machine
-// function getDayType(day) {
-//     switch (day.toLowerCase()) {
-//         case "friday":
-//         case "saturday":
-//             return "Weekend";
+// Question 2: Bangladesh Weekend Machine
+function getDayType(day) {
+    switch (day.toLowerCase()) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
 
-//         case "sunday":
-//         case "monday":
-//         case "tuesday":
-//         case "wednesday":
-//         case "thursday":
-//             return "Working Day";
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
 
-//         default:
-//             return "Invalid Day";
-//     }
-// }
-
-
+        default:
+            return "Invalid Day";
+    }
+}
 
 
 
-// // Question 1: 
-// function describeValue(value) {
-//     const type = typeof value;
-//     const truthiness = value ? "truthy" : "falsy";
 
-//     return `${type} | ${truthiness}`;
-// }
+
+// Question 1: 
+function describeValue(value) {
+    const type = typeof value;
+    const truthiness = value ? "truthy" : "falsy";
+
+    return `${type} | ${truthiness}`;
+}
 
